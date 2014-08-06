68ff0835588b8f07ca9a53fbc8bb1c58b3944bd3
dmitriy.savran <dmitriy.savran@faceit.com.ua>
2014-08-06 17:33:54 +0300

    fix

--- lib/generators/checkout_ru/install_generator.rb
+++ lib/generators/checkout_ru/install_generator.rb
@@ -1,7 +1,7 @@
 module CheckoutRu
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../lib", __FILE__)
+      source_root File.expand_path("", __FILE__)
       def copy_files
         copy_file "checkout_controller_decorator.rb", "app/controllers/spree/checkout_controller_decorator.rb"
         copy_file "checkout.js", "app/assets/javascripts/spree/frontend/checkoutru.js"
