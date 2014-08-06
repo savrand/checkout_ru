6d54d735fbce3381b902b5bd60309a12310a28d2
dmitriy.savran <dmitriy.savran@faceit.com.ua>
2014-08-06 17:15:17 +0300

    fix

--- lib/generators/checkout_ru/install_generator.rb
+++ lib/generators/checkout_ru/install_generator.rb
@@ -3,7 +3,9 @@ module CheckoutRu
     class InstallGenerator < Rails::Generators::Base
 
       def copy_files
-        p"test"
+        copy_file "../../../lib/checkout_controller_decorator.rb", "app/controllers/spree/checkout_controller_decorator.rb"
+        copy_file "../../../lib/checkout.js", "app/assets/javascripts/spree/frontend/checkoutru.js"
+        copy_file "../../../lib/_checkout_ru.html.erb", "app/views/spree/checkout/_checkout_ru.html.erb"
       end
 
     end
